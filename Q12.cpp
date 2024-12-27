f22690263b8c3f7dc71566e03311f70af445765b
Himanshu <himanshu.sde@gmail.com>
2024-12-27 12:27:40 +0530

    modified changes

--- Q12.cpp
+++ Q12.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #include<vector>
 // number of ways to express all elements of an array using + and - to lead to a target sum
-int numberofways(int index , vector<int>&nums , vector<int>&current, int sum , int target){
+int numberOfWays(int index , vector<int>&nums , int sum , int target){
     if(index == nums.size()){
         if(sum == target ){
             return 1;
@@ -11,9 +11,9 @@ int numberofways(int index , vector<int>&nums , vector<int>&current, int sum , i
         return 0;
     }
     //take 
-    int left = numberofways(index+1, nums, current , sum+nums[index] , target);
+    int left = numberOfWays(index+1, nums,  sum+nums[index] , target);
     //don't take 
-    int right = numberofways(index+1, nums, current , sum-nums[index] , target);
+    int right = numberOfWays(index+1, nums, sum-nums[index] , target);
     return left+ right;
     
 }
@@ -24,7 +24,7 @@ int main(){
     int sum = 0;
     int target =3;
     
-    cout<< numberofways(0 , nums, current , sum , target );
+    cout<< numberOfWays(0 , nums,  sum , target );
 
     return 0;
 }
